38f4dd14af0fabcc03e7907eb9aeda1fc0fbc851
GOODCATHUB <60478798+GOODCATHUB@users.noreply.github.com>
2020-01-30 19:23:31 +0300

    Самое начало

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form method="post" action="calc.php"><pre>
+ Сумма заимствования <input type="text" name="principle">
+ Ежемесячная выплата <input type="text" name="monthly">
+ Количество лет <input type="text" name="years" value="25">
+ Процент годовых <input type="text" name="rate" value="6">
+ <input type="submit">
+</pre></form>
+</body>
+</html>
